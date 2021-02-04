5edd0408cbb30a2113c2c2a71b99400723e96270
VitalyBaboshin <vitaliy2@ro.ru>
2021-02-04 13:53:44 +0300

    Предварительная структура роутинга, пока еще без Guard

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {LocationsComponent} from "./shared/pages/locations/locations.component";
+import {LocationComponent} from "./shared/pages/location/location.component";
+import {AboutComponent} from "./shared/pages/about/about.component";
+import {ReviewComponent} from "./dialog/review/review.component";
+import {AuthComponent} from "./shared/pages/auth/auth.component";
+import {RegisterComponent} from "./shared/pages/register/register.component";
+
+
+const routes: Routes = [
+  {path: '', redirectTo: '/',pathMatch: 'full'},
+  {path: '', component: LocationsComponent, pathMatch: 'full'},
+  {path: 'about', component: AboutComponent, pathMatch: 'full'},
+  {path: 'auth', component: AuthComponent, pathMatch: 'full'},
+  {path: 'register', component: RegisterComponent, pathMatch: 'full'},
+  {path: 'location', component: LocationComponent, pathMatch: 'full'},
+  {path: 'location/:id', component: LocationComponent, pathMatch: 'full'},
+  {path: 'location/review/new', component: ReviewComponent, pathMatch: 'full'},
+  {path: '**', redirectTo: '/'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
