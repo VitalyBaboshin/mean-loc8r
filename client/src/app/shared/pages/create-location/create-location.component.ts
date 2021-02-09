f2d4a1b621178762067c5bce0eca2016c315700b
VitalyBaboshin <vitaliy2@ro.ru>
2021-02-09 18:00:32 +0300

    Init Компонент для создания новой локации

---
+++ client/src/app/shared/pages/create-location/create-location.component.ts
@@ -0,0 +1,96 @@
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import * as M from 'materialize-css';
+
+import {HttpService} from '../../../services/http.service';
+import {AuthServices} from "../../../services/auth.services";
+import {LocationService} from "../../../services/location.service";
+
+
+@Component({
+  selector: 'app-create-location',
+  templateUrl: './create-location.component.html',
+  styleUrls: ['./create-location.component.scss']
+})
+export class CreateLocationComponent implements OnInit {
+  formCreateFacilities: FormGroup;
+  formLocation: FormGroup;
+  facilities = [];
+  reviewsDeafult = {author: '', rating: '', reviewText: ''};
+  reviews = {author: '', rating: '', reviewText: ''};
+  facilititesActive = [];
+  send = {};
+  daysDefault = [{days: 'Monday-Friday', opening: '', closing: '', closed: false},
+                {days: 'Saturday', opening: '', closing: '', closed: false},
+                {days: 'Sunday', opening: '', closing: '', closed: false}];
+  days = [{days: 'Monday-Friday', opening: '', closing: '', closed: false},
+          {days: 'Saturday', opening: '', closing: '', closed: false},
+          {days: 'Sunday', opening: '', closing: '', closed: false}];
+
+  constructor(private http: HttpService,
+              private auth: AuthServices,
+              private location: LocationService) { }
+
+  ngOnInit(): void {
+    M.updateTextFields();
+    this.formCreateFacilities = new FormGroup({
+      nameFacilities: new FormControl(null, [Validators.required])
+    });
+    this.formLocation = new FormGroup({
+      name: new FormControl(null, [Validators.required]),
+      address: new FormControl(null, [Validators.required]),
+      rating: new FormControl(null)
+    });
+
+    this.http.getFacilities().subscribe(data => {
+        this.facilities = data;
+        this.facilititesActive = Array(this.facilities.length).fill(false);
+    });
+  }
+
+  createFacilities() {
+
+    this.http.createFacilities(this.formCreateFacilities.value).subscribe(
+      data => {
+        M.toast({html: data.message});
+      }, error => {
+        M.toast({html: error.error.message});
+      }
+    );
+  }
+
+
+  checkFacility(i: number) {
+    this.facilititesActive[i] = !this.facilititesActive[i];
+  }
+
+  checked(i: number) {
+    this.days[i].closed = !this.days[i].closed;
+  }
+
+  Publish() {
+    this.send = this.formLocation.value;
+    this.send['facilities'] = this.facilititesActive.map((item, index) => {
+      if (item) {
+        return this.facilities[index];
+      }
+    }).filter(Boolean);
+    this.send['openingTimes'] = this.days;
+
+    this.reviews.author = this.auth.getName();
+    this.reviews.rating = this.formLocation.value.rating;
+
+    this.send['reviews'] = [this.reviews];
+    this.send['coords'] = [this.location.MapPoint.latitude, this.location.MapPoint.longitude];
+
+
+    this.http.create(this.send).subscribe( data => {
+        M.toast({html: data.message});
+        this.formLocation.reset();
+        this.days = this.daysDefault;
+        this.facilititesActive = [];
+        this.reviews = this.reviewsDeafult;
+    }
+    );
+  }
+}
