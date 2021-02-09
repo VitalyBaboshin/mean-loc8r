4232354a1476e26f11b3c6c8d1f7eb40f0fe044c
VitalyBaboshin <vitaliy2@ro.ru>
2021-02-09 17:58:03 +0300

    При logout переход на начальную страницу, Добавлен materialize dropdown

--- client/src/app/shared/component/navbar/navbar.component.ts
+++ client/src/app/shared/component/navbar/navbar.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import {AuthServices} from "../../../services/auth.services";
+import {Component, ElementRef,  OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AuthServices} from '../../../services/auth.services';
 
+import * as M from 'materialize-css';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit{
+
+  @ViewChild('dropdown', {static: true}) elem: ElementRef;
 
   userName: string;
 
-  constructor(private auth: AuthServices) {
+
+  constructor(private auth: AuthServices,
+              private router: Router) {
     this.userName = this.auth.getName();
   }
 
   ngOnInit(): void {
     this.auth.name$.subscribe((name) => {
       this.userName = name;
-    })
+    });
+    M.Dropdown.init(this.elem.nativeElement, { alignment: 'right'});
+
   }
 
   logOut() {
     this.auth.logout();
+    this.router.navigate(['/']);
   }
 }
