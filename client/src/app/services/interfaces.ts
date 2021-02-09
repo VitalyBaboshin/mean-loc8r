698f19a0686a06af3dc87e9ec9b27f6b83b74e52
VitalyBaboshin <vitaliy2@ro.ru>
2021-02-09 17:54:23 +0300

    Раскомментировал координаты в интерфейсе локация

--- client/src/app/services/interfaces.ts
+++ client/src/app/services/interfaces.ts
@@ -1,29 +1,29 @@
 export interface User {
-  email: string,
-  password: string
+  email: string;
+  password: string;
 }
 
 export interface Review {
-  author: string,
-  rating: number,
-  reviewText: string,
-  createdOn: Date
+  author: string;
+  rating: number;
+  reviewText: string;
+  createdOn?: Date;
 }
 
 export interface OpeningTime{
-  days: string,
-  opening: string,
-  closing: string,
-  closed: boolean
+  days: string;
+  opening: string;
+  closing: string;
+  closed: boolean;
 }
 
 export interface Location {
-  name: string,
-  address:  string,
-  rating: number,
-  facilities: [string],
-  // coords: {type: [Number], index: '2dsphere'},
-  openingTimes: [OpeningTime],
-  reviews: [Review],
-  _id: string
+  name: string;
+  address: string;
+  rating: number;
+  facilities: [string];
+  coords: [number];
+  openingTimes: [OpeningTime];
+  reviews: [Review];
+  _id: string;
 }
