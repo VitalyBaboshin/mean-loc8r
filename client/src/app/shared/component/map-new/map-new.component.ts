ebd36b413c350731f5db4a609c158f86966ab97c
VitalyBaboshin <vitaliy2@ro.ru>
2021-02-09 17:56:49 +0300

    Init компонент для выбора местоположения при создании новой локации

---
+++ client/src/app/shared/component/map-new/map-new.component.ts
@@ -0,0 +1,111 @@
+import {Component, OnInit, Output} from '@angular/core';
+import {icon, latLng, LeafletMouseEvent, Map, MapOptions, marker, Marker, tileLayer} from 'leaflet';
+import * as M from 'materialize-css';
+import {MapPoint} from '../../../models/map-point.model';
+import {LocationService} from '../../../services/location.service';
+
+
+@Component({
+  selector: 'app-map-new',
+  templateUrl: './map-new.component.html',
+  styleUrls: ['./map-new.component.scss']
+})
+export class MapNewComponent implements OnInit {
+  mapPoint: MapPoint;
+  map: Map;
+  options: MapOptions;
+  lastLayer: any;
+  enableDraw = false;
+  coords = [];
+
+  constructor(private locationService: LocationService) {}
+
+  ngOnInit() {
+
+    // Проверяем включена ли геолокация на устройстве
+    navigator.geolocation.getCurrentPosition(data => {
+      this.locationService.MapPoint = {latitude: data.coords.latitude, longitude: data.coords.longitude};
+      // this.mapPoint = {
+      //   latitude: data.coords.latitude,
+      //   longitude: data.coords.longitude
+      // };
+      this.enableDraw = true;
+      this.locationService.setStateLocation(true);
+      this.initializeMapOptions();
+    }, (error) => {
+      if (error.PERMISSION_DENIED) {
+        M.toast({html: 'Для доступа ко всему функционалу приложения включите геолокацию и перезапустите'}, );
+        this.locationService.setStateLocation(false);
+        this.enableDraw = true;
+        this.initializeDefaultMapPoint();
+        this.initializeMapOptions();
+      }
+    });
+  }
+
+  private initializeMapOptions() {
+
+    this.options = {
+      zoom: 13,
+      layers: [
+        tileLayer(
+          'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          {
+            maxZoom: 18,
+            attribution: 'Map data © OpenStreetMap contributors'
+          })
+      ],
+    };
+  }
+
+// По дефолту координаты любимого города Санкт-Петербург
+  private initializeDefaultMapPoint() {
+    this.locationService.MapPoint = {latitude: 59.94, longitude: 30.31};
+    // this.mapPoint = {
+    //   latitude: 59.94,
+    //   longitude: 30.31
+    // };
+  }
+
+  initializeMap(map: Map) {
+    this.map = map;
+    this.createMarker();
+  }
+
+  public onMapClick(e: LeafletMouseEvent) {
+    this.clearMap();
+    this.updateMapPoint(e.latlng.lat, e.latlng.lng);
+    this.createMarker();
+
+  }
+  private clearMap() {
+    if (this.map.hasLayer(this.lastLayer)) { this.map.removeLayer(this.lastLayer); }
+  }
+  private updateMapPoint(latitude: number, longitude: number) {
+    this.locationService.MapPoint = {latitude, longitude};
+    // this.mapPoint = {
+    //   latitude,
+    //   longitude
+    // };
+  }
+  private createMarker() {
+    this.clearMap();
+    const mapIcon = this.getDefaultIcon();
+    const coordinates = latLng(
+      [this.locationService.MapPoint.latitude,
+             this.locationService.MapPoint.longitude]);
+    // const coordinates = latLng([this.mapPoint.latitude, this.mapPoint.longitude]);
+    this.lastLayer = marker(coordinates).setIcon(mapIcon).addTo(this.map);
+    this.map.setView(coordinates, this.map.getZoom());
+  }
+  private getDefaultIcon() {
+    return icon({
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
+      iconUrl: 'assets/marker-icon.png'
+    });
+  }
+
+
+
+}
