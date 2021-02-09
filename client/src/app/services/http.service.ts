c19c0ff53942d2b912b1216e381535bc6a36c978
VitalyBaboshin <vitaliy2@ro.ru>
2021-02-09 17:53:35 +0300

    Добавлено post сооздание координат для локации, post создание особенности, получение всех особенностей

--- client/src/app/services/http.service.ts
+++ client/src/app/services/http.service.ts
@@ -1,41 +1,51 @@
-import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable, timer} from "rxjs";
-import {AuthServices} from "./auth.services";
-import {mergeMap} from "rxjs/operators";
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, timer} from 'rxjs';
+
+import {mergeMap} from 'rxjs/operators';
 
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 
 export class HttpService {
-  readonly URL_API = "/api/locations"
+  readonly URL_API = '/api/locations';
+  readonly URL_API_ADMIN = '/api/admin';
   constructor(private http: HttpClient) {}
   //
   public create(location: any): Observable<any> {
-    return this.http.post(`${this.URL_API}`, location)
+    return this.http.post(`${this.URL_API}`, location);
   }
 
-  //todo оставить этот код
-  // public getLocation(locationId: string): Observable<any> {
-  //   return this.http.get(`${this.URL_API}/${locationId}`)
-  // }
-  //todo убрать задержку
+  // todo оставить этот код
   public getLocation(locationId: string): Observable<any> {
-    return timer(1000).pipe(
-      mergeMap(() => this.http.get(`${this.URL_API}/${locationId}`))
-    )
+    return this.http.get(`${this.URL_API}/${locationId}`);
   }
+  // todo убрать задержку
+  // public getLocation(locationId: string): Observable<any> {
+  //   return timer(1000).pipe(
+  //     mergeMap(() => this.http.get(`${this.URL_API}/${locationId}`))
+  //   );
+  // }
   //
   // public deleteLink(link: string): Observable<any> {
   //   return this.http.delete(`${this.URL_API}/${link}`)
   // }
   //
   public getAllLocations(): Observable<any> {
-    return this.http.get(`${this.URL_API}`)
+    return this.http.get(`${this.URL_API}`);
   }
   public createReview(locationId: string, review: any): Observable<any> {
-    return this.http.post(`${this.URL_API}/${locationId}/reviews`, review)
+    return this.http.post(`${this.URL_API}/${locationId}/reviews`, review);
+  }
+  public createCoordLocation(locationId: string, coords: [number, number]): Observable<any> {
+    return this.http.post(`${this.URL_API}/${locationId}/coords`, coords);
+  }
+  public createFacilities(name: string): Observable<any> {
+    return this.http.post(`${this.URL_API_ADMIN}/facilities`, name);
+  }
+  public getFacilities(): Observable<any> {
+    return this.http.get(`${this.URL_API_ADMIN}/facilities`);
   }
 }
