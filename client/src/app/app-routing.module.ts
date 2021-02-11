2900cb9e405f2db8dc481a1d76b08b57d0896295
VitalyBaboshin <vitaliy2@ro.ru>
2021-02-11 18:06:17 +0300

    Add guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LocationsComponent} from "./shared/pages/locations/locations.component";
-import {LocationComponent} from "./shared/pages/location/location.component";
-import {AboutComponent} from "./shared/pages/about/about.component";
-import {ReviewComponent} from "./dialog/review/review.component";
-import {AuthComponent} from "./shared/pages/auth/auth.component";
-import {RegisterComponent} from "./shared/pages/register/register.component";
-import {CreateLocationComponent} from "./shared/pages/create-location/create-location.component";
+import {LocationsComponent} from './shared/pages/locations/locations.component';
+import {LocationComponent} from './shared/pages/location/location.component';
+import {AboutComponent} from './shared/pages/about/about.component';
+import {ReviewComponent} from './dialog/review/review.component';
+import {AuthComponent} from './shared/pages/auth/auth.component';
+import {RegisterComponent} from './shared/pages/register/register.component';
+import {CreateLocationComponent} from './shared/pages/create-location/create-location.component';
+import {AuthGuard} from './services/classes/auth.guard';
 
 
 const routes: Routes = [
@@ -16,9 +17,9 @@ const routes: Routes = [
   {path: 'auth', component: AuthComponent, pathMatch: 'full'},
   {path: 'register', component: RegisterComponent, pathMatch: 'full'},
   {path: 'location', component: LocationComponent, pathMatch: 'full'},
-  {path: 'location/new', component: CreateLocationComponent, pathMatch: 'full'},
+  {path: 'location/new', component: CreateLocationComponent, canActivate: [AuthGuard], pathMatch: 'full'},
   {path: 'location/:id', component: LocationComponent, pathMatch: 'full'},
-  {path: 'location/review/new', component: ReviewComponent, pathMatch: 'full'},
+  {path: 'location/review/new', component: ReviewComponent, canActivate: [AuthGuard], pathMatch: 'full' },
   {path: '**', redirectTo: '/'}
 ];
 
